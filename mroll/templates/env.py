3475c5c9c112ee85733a734659339227f2cd7627
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-05-11 12:06:01 +0200

    escapings

--- mroll/templates/env.py
+++ mroll/templates/env.py
@@ -1,6 +1,5 @@
 import pymonetdb
 from pymonetdb import Error as PyMonetdbErr
-from pymonetdb.sql import monetize
 import configparser
 import os
 
@@ -12,14 +11,14 @@ db_name = config['db']['db_name']
 user = config['db']['user']
 password = config['db']['password']
 port = config['db']['port']
-tbl_name = monetize.convert(config['mroll']['rev_history_tbl_name']).replace('\'', '\"')
+tbl_name = config['mroll']['rev_history_tbl_name']
 
 
 def get_head(db_name=db_name, tbl_name=tbl_name):
     rev = None
     conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
     try:
-        sql = "select id, description, ts from {} as r where r.ts=(select max(ts) from {})".format(tbl_name, tbl_name)
+        sql = """select id, description, ts from sys."{}" as r where r.ts=(select max(ts) from sys."{}")""".format(tbl_name, tbl_name)
         curr = conn.cursor()
         curr.execute(sql)
         rev = curr.fetchone()
@@ -30,8 +29,8 @@ def get_head(db_name=db_name, tbl_name=tbl_name):
 def create_revisions_table(db_name=db_name, tbl_name=tbl_name):
     conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
     sql = """
-    create table if not exists {}(id string, description string, ts timestamp);
-    alter table {} add constraint mroll_rev_pk primary key (id);
+    create table if not exists sys."{}"(id string, description string, ts timestamp);
+    alter table sys."{}" add constraint mroll_rev_pk primary key (id);
     """.format(tbl_name, tbl_name)
     try:
         conn.execute(sql)
@@ -46,7 +45,7 @@ def create_revisions_table(db_name=db_name, tbl_name=tbl_name):
 
 def get_revisions(db_name=db_name, tbl_name=tbl_name):
     conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
-    sql = "select id, description, ts from {} order by ts".format(tbl_name)
+    sql = """select id, description, ts from sys."{}" order by ts""".format(tbl_name)
     cur = conn.cursor()
     try:
         cur.execute(sql)
@@ -59,12 +58,13 @@ def add_revision(id_, description, ts, upgrade_sql, db_name=db_name, tbl_name=tb
     Applies the upgrade sql and adds it to meta data in one transaction
     """
     conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+    cur = conn.cursor()
     sql = """
-    insert into {} values ('{}', '{}', '{}')
-    """.format(tbl_name, id_, description, ts)
+    insert into sys."{}" values (%s, %s, %s)
+    """.format(tbl_name)
     try:
         conn.execute(upgrade_sql)
-        conn.execute(sql)
+        cur.execute(sql, (id_, description, ts))
         conn.commit()
         return True
     except Exception as e:
@@ -76,10 +76,11 @@ def add_revision(id_, description, ts, upgrade_sql, db_name=db_name, tbl_name=tb
 
 def remove_revision(id_, downgrade_sql, db_name=db_name, tbl_name=tbl_name):
     conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
-    sql = "delete from {} where id='{}'".format(tbl_name, id_)
+    cur = conn.cursor()
+    sql = """delete from sys."{}" where id=%s""".format(tbl_name)
     try:
         conn.execute(downgrade_sql)
-        conn.execute(sql)
+        cur.execute(sql, (id_,))
         conn.commit()
         return True
     except Exception as e:
