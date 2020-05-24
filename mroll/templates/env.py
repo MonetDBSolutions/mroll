893ddf9f1053abcce37ebb9e2bf96b2325c1a605
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-05-24 23:07:02 +0200

    env fixes

--- mroll/templates/env.py
+++ mroll/templates/env.py
@@ -1,5 +1,4 @@
 import pymonetdb
-from pymonetdb import Error as PyMonetdbErr
 import configparser
 import os
 
@@ -7,16 +6,18 @@ config = configparser.ConfigParser()
 dir_ = os.path.dirname(__file__)
 configuration = os.path.join(dir_, 'mroll.ini')
 config.read(configuration)
+
 db_name = config['db']['db_name']
-user = config['db']['user']
+username = config['db']['username']
 password = config['db']['password']
-port = config['db']['port']
+hostname = config['host']['hostname']
+port = config['host']['port']
 tbl_name = config['mroll']['rev_history_tbl_name']
 
 
-def get_head(db_name=db_name, tbl_name=tbl_name):
+def get_head(db_name=db_name, hostname=hostname, port=port, username=username, password=password, tbl_name=tbl_name):
     rev = None
-    conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+    conn = pymonetdb.connect(db_name, hostname=hostname, port=port, username=username, password=password)
     try:
         sql = """select id, description, ts from sys."{}" as r where r.ts=(select max(ts) from sys."{}")""".format(tbl_name, tbl_name)
         curr = conn.cursor()
@@ -26,8 +27,8 @@ def get_head(db_name=db_name, tbl_name=tbl_name):
         conn.close() 
     return rev
 
-def create_revisions_table(db_name=db_name, tbl_name=tbl_name):
-    conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+def create_revisions_table(db_name=db_name, hostname=hostname, port=port, username=username, password=password, tbl_name=tbl_name):
+    conn = pymonetdb.connect(db_name, hostname=hostname, port=port, username=username, password=password)
     sql = """
     create table sys."{}"(id string, description string, ts timestamp);
     alter table sys."{}" add constraint mroll_rev_pk primary key (id);
@@ -35,14 +36,14 @@ def create_revisions_table(db_name=db_name, tbl_name=tbl_name):
     try:
         conn.execute(sql)
         conn.commit()
-    except PyMonetdbErr as e:
+    except Exception as e:
         conn.rollback()
         raise(e)    
     finally:
         conn.close()
 
-def get_revisions(db_name=db_name, tbl_name=tbl_name):
-    conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+def get_revisions(db_name=db_name, hostname=hostname, port=port, username=username, password=password, tbl_name=tbl_name):
+    conn = pymonetdb.connect(db_name, hostname=hostname, port=port, username=username, password=password)
     sql = """select id, description, ts from sys."{}" order by ts""".format(tbl_name)
     cur = conn.cursor()
     try:
@@ -51,11 +52,13 @@ def get_revisions(db_name=db_name, tbl_name=tbl_name):
     finally:
         conn.close()
 
-def add_revision(id_, description, ts, upgrade_sql, db_name=db_name, tbl_name=tbl_name):
+def add_revision(id_, description, ts, upgrade_sql, 
+    db_name=db_name, hostname=hostname, port=port, 
+    username=username, password=password, tbl_name=tbl_name):
     """
     Applies the upgrade sql and adds it to meta data in one transaction
     """
-    conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+    conn = pymonetdb.connect(db_name, hostname=hostname, port=port, username=username, password=password)
     cur = conn.cursor()
     sql = """
     insert into sys."{}" values (%s, %s, %s)
@@ -64,26 +67,24 @@ def add_revision(id_, description, ts, upgrade_sql, db_name=db_name, tbl_name=tb
         conn.execute(upgrade_sql)
         cur.execute(sql, (id_, description, ts))
         conn.commit()
-        return True
     except Exception as e:
         conn.rollback()
         raise(e)
     finally:
         conn.close()
-    return False
 
-def remove_revision(id_, downgrade_sql, db_name=db_name, tbl_name=tbl_name):
-    conn = pymonetdb.connect(db_name, port=port, username=user, password=password)
+def remove_revision(id_, downgrade_sql, 
+    db_name=db_name, hostname=hostname, port=port, 
+    username=username, password=password, tbl_name=tbl_name):
+    conn = pymonetdb.connect(db_name, hostname=hostname, port=port, username=username, password=password)
     cur = conn.cursor()
     sql = """delete from sys."{}" where id=%s""".format(tbl_name)
     try:
         conn.execute(downgrade_sql)
         cur.execute(sql, (id_,))
         conn.commit()
-        return True
     except Exception as e:
         conn.rollback()
         raise(e)
     finally:
         conn.close()
-    return False
